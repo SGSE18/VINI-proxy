84efa56200245a69c3f89830e05b038bff508673
BreakBB <b.boeing@online.de>
2018-06-13 19:53:13 +0200

    Add error reporting and certificates for local testing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 const fs = require('fs');
-const proxy = require('http-proxy');
+const httpProxy = require('http-proxy');
 
 const proxyPort = 8899;
 
-proxy.createServer({
+let pathToKey = process.env['HOME'] + '/.ssh/domain.key';
+let pathToCert = process.env['HOME'] + '/.ssh/domain.crt';
+
+if(process.env['HOME'] == null || process.env['HOME'] === undefined){
+  pathToKey = "../cert/domain_test.key";
+  pathToCert = "../cert/domain_test.crt";
+}
+
+const proxy = httpProxy.createServer({
   target: {
-    host: 'localhost',
-    port: 3311
+    host: 'http://localhost:3311'
   },
   ssl: {
-    key: fs.readFileSync(process.env['HOME'] + '/.ssh/domain.key', 'utf8'),
-    cert: fs.readFileSync(process.env['HOME'] + '/.ssh/domain.crt', 'utf8')
-  }
+    key: fs.readFileSync(pathToKey, 'utf8'),
+    cert: fs.readFileSync(pathToCert, 'utf8')
+  },
+  changeOrigin: true,
+  secure: false
 }).listen(proxyPort);
+
+proxy.on('error', (err, req, res) => {
+  console.log("An error occurred:", err);
+  res.status(500);
+  res.json({
+    "message": "An wild error occurred",
+    "error": err
+  })
+});
 
 console.log('Proxy to http://localhost:3311 is listening on port', proxyPort);
